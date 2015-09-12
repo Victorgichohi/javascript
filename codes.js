95ea1c6e8b8beef15dd97ae243a90a315206c192
victorgichohi <vitorgichohi48@gmail.com>
2015-09-12 22:56:04 +0300

    loop function,modification coming up soon

--- codes.js
+++ codes.js
@@ -1,3 +1,18 @@
-function touch () {
-    alert("ouch");
+function person(name, age) {
+    this.name = name1;
+    this.age = age1;
+    this.underage = yrsold;
 }
+function yrsold(){
+    var yrsleft=18-this.age;
+    return yrsleft;
+}
+name1=prompt("Enter name");
+age1=prompt ("Enter your age");
+if (age1 >21){
+document.write("your grown enough,booze is your breakfast");
+}else{
+    
+    document.write (person.retire);
+}
+
